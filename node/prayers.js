4321dc573d4fe32fa292898d4527f186ffce065f
Carson Long <carsonthelong@gmail.com>
2018-01-15 04:40:23 -0800

    (feat/prettify): fixed prayers indentation

--- node/prayers.js
+++ node/prayers.js
@@ -1,41 +1,60 @@
 
 const prayers = [
-`Lord, you said that when two or three would gather together in your name, then you would be present with them.
+  `
+Lord, you said that when two or three would gather together in your name, then you would be present with them.
 I am praying by myself (or ‘on the Internet’) but I am uniting myself
 with many individual Christians throughout the world
 who, though separate,
 are gathered together in another sense to pray to you,
-and I trust that you are with me now.`,
-`God our heavenly Father, when the thought of you wakes in our hearts,
+and I trust that you are with me now.
+  `,
+  `
+God our heavenly Father, when the thought of you wakes in our hearts,
 let its awakening
 not be like a startled bird
 that flies about in fear. Instead, let it be like a child
-waking from sleep with a heavenly smile.`,
-`O Christ, our Morning Star,
+waking from sleep with a heavenly smile.
+  `,
+  `
+O Christ, our Morning Star,
 Splendour of Light Eternal,
 shining with the glory of the rainbow, come and waken us
 from the greyness of our apathy,
-and renew in us your gift of hope. Amen.`,
-`Come, Holy Spirit, fill the hearts of your faithful, and enkindle in them the fire of your love.
+and renew in us your gift of hope. Amen.
+  `,
+  `
+Come, Holy Spirit, fill the hearts of your faithful, and enkindle in them the fire of your love.
 Send forth your Spirit and they shall be created And you shall renew the face of the earth.
 O God, who has taught the hearts of the faithful by the light of the Holy Spirit,
 grant that by the gift of the same Spirit
 we may be always truly wise
-and ever rejoice in his consolation. Through Christ our Lord. Amen.`,
-`Lord God, Creator of light,
-at the rising of your sun each morning, let the greatest of all lights - your love - rise, like the sun, within my heart.`,
-`Be, Lord Jesus, a bright flame before me, a guiding star above me,
+and ever rejoice in his consolation. Through Christ our Lord. Amen.
+  `,
+  `
+Lord God, Creator of light,
+at the rising of your sun each morning, let the greatest of all lights - your love - rise, like the sun, within my heart.
+  `,
+  `
+Be, Lord Jesus, a bright flame before me, a guiding star above me,
 a smooth path below me,
 a kindly shepherd behind me:
-today, tonight, and forever.`,
-`O Lord,
+today, tonight, and forever.
+  `,
+  `
+O Lord,
 you know how busy I must be this day. If I forget you,
-do not forget me.`,
-`Lord, the sea is so wide and my boat is so small. Be with me.`,
-`Lord, you are closer to me
+do not forget me.
+  `,
+  `
+Lord, the sea is so wide and my boat is so small. Be with me.
+  `,
+  `
+Lord, you are closer to me
 than my own breathing,
-nearer than my hands and feet.`,
-`Lord Jesus,
+nearer than my hands and feet.
+  `,
+  `
+Lord Jesus,
 my hands to do your work.
 my feet to go your way.
 my eyes to see as you do.
@@ -51,8 +70,10 @@ I give you
 your Father and all mankind.
 I give you my whole self that you may grow in me, so that it is you, Lord Jesus,
 my heart that you may love in me
-who live and work and pray in me.`,
-`Lord Jesus,
+who live and work and pray in me.
+  `,
+  `
+Lord Jesus,
 you told your friends
 not to worry about the future.
 You showed them
@@ -71,20 +92,26 @@ but also the suffering and sacrifices
 that I want to offer cheerfully and lovingly, and in a quiet and hidden way.
 And so may any difficulties
 and frustration and pain of this day be transformed in your presence
-for the benefit of other people. Amen.`,
-`I thank you, Lord,
+for the benefit of other people. Amen.
+    `,
+  `
+I thank you, Lord,
 for knowing me better than I know myself, and for letting me know myself
 better than others know me.
 Make me, I pray you,
 better than they suppose,
-and forgive me for what they do not know.`,
-`I bind unto myself today
+and forgive me for what they do not know.
+  `,
+  `
+I bind unto myself today
 The power of God to hold and lead, His eye to watch, his might to stay, His ear to hearken to my need.
 The wisdom of my God to teach,
 His hand to guide, his shield to ward; The word of God to give me speech, His heavenly host to be my guard.
 Christ be with me, Christ within me, Christ behind me, Christ before me, Christ beside me, Christ to win me, Christ to comfort and restore me. Christ beneath me, Christ above me, Christ in quiet, Christ in danger,
-Christ in hearts of all that love me, Christ in mouth of friend and stranger.`,
-`God our Father,
+Christ in hearts of all that love me, Christ in mouth of friend and stranger.
+  `,
+  `
+God our Father,
 the qualities I see lived out so well in some people are a reflection of your own goodness,
 and I know
 that I have much to learn from other people
@@ -92,22 +119,30 @@ who reflect your image and likeness
 in different ways.
 Inspire me
 to respect others fully as my equals, seeing and loving in them
-what you see and love in them.`,
-`O Lord my God.
+what you see and love in them.
+  `,
+  `
+O Lord my God.
 Teach my heart this day where and how to find you.
 You have made me and re-made me, and you have bestowed on me
 all the good things I possess,
 and still I do not know you.
 I have not yet done
-that for which I was made.`,
-`Teach me to seek you, for I cannot seek you unless you teach me, or find you
+that for which I was made.
+  `,
+  `
+Teach me to seek you, for I cannot seek you unless you teach me, or find you
 unless you show yourself to me.
-Let me seek you in my desire; let me desire you in my seeking. Let me find you by loving you; let me love you when I find you.`,
-`O God,
+Let me seek you in my desire; let me desire you in my seeking. Let me find you by loving you; let me love you when I find you.
+  `,
+  `
+O God,
 may the fire of the Holy Spirit burn up the dross in our hearts, warm them with love,
 and set them on fire
-with zeal for your service. Amen.`,
-`O Lord,
+with zeal for your service. Amen.
+  `,
+  `
+O Lord,
 you have mercy on all,
 take away from me my sins,
 and mercifully set me ablaze with the fire of your Holy Spirit.
@@ -117,8 +152,10 @@ a heart to delight in you,
 to follow and enjoy you. Amen.
 Teach me to seek you, for I cannot seek you unless you teach me, or find you
 unless you show yourself to me.
-Let me seek you in my desire; let me desire you in my seeking. Let me find you by loving you; let me love you when I find you.`,
-`God our Father,
+Let me seek you in my desire; let me desire you in my seeking. Let me find you by loving you; let me love you when I find you.
+  `,
+  `
+God our Father,
 until the time of the printing press, people copied the Gospel,
 writing it by hand.
 Slowly the Gospel took shape - both on the page
@@ -133,8 +170,10 @@ as the pages of my own life turn over, remind me
 that you write my name
 on the palm of your hand.
 I ask this prayer through Jesus, who is your Word,
-living amongst us. Amen.`,
-`God our Father,
+living amongst us. Amen.
+  `,
+  `
+God our Father,
 if I could trace back
 through the last two thousand years, marking out routes
 from Jesus himself
@@ -155,22 +194,28 @@ for those who lived their faith
 through difficulties
 and hardship and persecution. I pray, Father, that I may grow
 in your faith and love
-through good times and bad. Amen.`,
-`God our Father,
+through good times and bad. Amen.
+  `,
+  `
+God our Father,
 you call each of us by name,
 and you treasure each of us individually as though no-one else exists.
 Inspire us
 to respect and value
 each person who comes into our lives this day.
-Amen.`,
-`Lord, we come before you as we are. We ask you to take away from us
+Amen.
+  `,
+  `
+Lord, we come before you as we are. We ask you to take away from us
 all that makes us less than human. Strengthen us with the power of your Spirit
 that our attitude and outlook may develop, and our “way of looking”
 may become more like yours.
 Help us to remain positive -
 encouraging and appreciating one another,
-looking upon people in the same way that you do. Amen.`,
-`Lord Jesus,
+looking upon people in the same way that you do. Amen.
+  `,
+  `
+Lord Jesus,
 I ask you to open my eyes
 as you did with the blind man, so that I may really see.
 Tune my ears
@@ -178,15 +223,21 @@ as you did with the man who was deaf and dumb, so that I may really hear
 what you are saying to me.
 May the many experiences of my senses remind me to be aware of others
 and of all that is around me.
-May all that I experience lead me closer to you.`,
-`Grant me to recognise in others, Lord God, the radiance of your own face.`,
-`God our Father,
+May all that I experience lead me closer to you.
+  `,
+  `
+Grant me to recognise in others, Lord God, the radiance of your own face.
+  `,
+  `
+God our Father,
 extend our horizons,
 widen our vision,
 and remind us how inter-connected we are as your sons and daughters.
 Breathe your Spirit into us that we may live more truly as brothers and sisters
-of one another. Amen.`,
-`Lord, we ask you to open our eyes
+of one another. Amen.
+  `,
+  `
+Lord, we ask you to open our eyes
 that we may value and appreciate all people, recognising what we have in common
 rather than focusing
 on what our differences might be.
@@ -195,11 +246,15 @@ between what is important
 and what is not,
 and open our minds and hearts
 that we may always be people of good will
-who bring life and joy to others. Amen.`,
-`Lead me, Lord,
+who bring life and joy to others. Amen.
+  `,
+  `
+Lead me, Lord,
 to recognise you
-in the person beside me.`,
-`God be in my head
+in the person beside me.
+  `,
+  `
+God be in my head
 and in my understanding. God be in my eyes
 and in my looking.
 God be in my mouth
@@ -207,14 +262,18 @@ and in my speaking.
 God be in my heart
 and in my thinking.
 God be at my end
-and at my departing.`,
-`God to enfold me, God to surround me, God in my speaking, God in my thinking.
+and at my departing.
+  `,
+  `
+God to enfold me, God to surround me, God in my speaking, God in my thinking.
 God in my sleeping, God in my waking, God in my watching, God in my hoping.
 God in my life, God in my lips, God in my soul, God in my heart.
 God in my sufficing,
 God in my slumber,
-God in my ever-living soul, God in mine eternity.`,
-`Lord, inspire me to give of my best and make good use
+God in my ever-living soul, God in mine eternity.
+  `,
+  `
+Lord, inspire me to give of my best and make good use
 of the talents you have given me.
 Show me how to be positive in attitude, appreciating and valuing others, always being ready to encourage
 and give praise.
@@ -225,8 +284,10 @@ might count as a great “success”
 of someone with other talents. Lead me never to judge people
 but to accept others as they are, knowing that it is together,
 each with our differences,
-that we build up your Kingdom. Amen.`,
-`Lord, I commit my failures
+that we build up your Kingdom. Amen.
+  `,
+  `
+Lord, I commit my failures
 as well as my successes
 into your hands,
 and I bring for your healing the people and the situations, the wrongs and the hurts
@@ -237,13 +298,19 @@ leaving the past behind me,
 and living the present to the full.
 Lead me always to be positive
 as I ‘entrust the past to your mercy, the present to your love,
-and the future to your providence’.`,
-`O God,
+and the future to your providence’.
+  `,
+  `
+O God,
 teach me
-to breathe deeply in faith.`,
-`Lord Jesus Christ, Son of God,
-have mercy on me, a sinner.`,
-`Penetrate these murky corners
+to breathe deeply in faith.
+  `,
+  `
+Lord Jesus Christ, Son of God,
+have mercy on me, a sinner.
+  `,
+  `
+Penetrate these murky corners
 where we hide our memories
 and tendencies on which we do not care to look, but which we will not yield freely up to you, that you may purify and transmute them.
 The persistent buried grudge,
@@ -254,14 +321,18 @@ the private comfort we cling to, the secret fear of failure which saps our initi
 and is really inverted pride,
 the pessimism which is an insult to your joy. Lord, we bring all these to you,
 and we review them
-with shame and penitence in your steadfast light.`,
-`Lord, I acknowledge my sinfulness. I ask you to empower me
+with shame and penitence in your steadfast light.
+  `,
+  `
+Lord, I acknowledge my sinfulness. I ask you to empower me
 with your Holy Spirit,
 that I may resist temptation and “choose life
 rather than death”,
 - good rather than evil -
-in the ordinary circumstances of my daily life. Amen.`,
-`Loving Lord,
+in the ordinary circumstances of my daily life. Amen.
+  `,
+  `
+Loving Lord,
 you tell us in the Bible
 that whatever wrong we have done you tread down our faults
 to the bottom of the sea.
@@ -278,8 +349,10 @@ to love and forgive.
 Lead us to be generous
 in accepting and forgiving others
 in the same way
-as you accept and forgive us. Amen.`,
-`Father,
+as you accept and forgive us. Amen.
+  `,
+  `
+Father,
 perhaps you have best been described as “love” and, as “love keeps no record of wrongs”,
 I rejoice in the completeness
 of your personal love and acceptance
@@ -287,8 +360,10 @@ of each one of us.
 Lead me each day to be faithful and grow closer to Jesus
 who calls me his friend.
 It is through him and in him, Father,
-that I am enabled to see you most fully: you who call me by name.`,
-`Lord, you are like a shepherd to me, and so I have all that I need.
+that I am enabled to see you most fully: you who call me by name.
+  `,
+  `
+Lord, you are like a shepherd to me, and so I have all that I need.
 You give me rest
 in meadows of green grass,
 and you lead me to water
@@ -308,14 +383,18 @@ and you remind me
 that I am significant and special.
 You call me to goodness and kindness every day of my life,
 and your house will be my home
-my whole life long.`,
-`Lord, inspire me to live in such a way
+my whole life long.
+  `,
+  `
+Lord, inspire me to live in such a way
 that my choices each day
 and my commitment to live in a positive way may transform
 the negative into something positive,
 and the ordinariness of daily life
-into something extraordinary. Amen.`,
-`God our Father,
+into something extraordinary. Amen.
+  `,
+  `
+God our Father,
 may no-one’s negative actions ever overpower my determination to choose to live in a positive way.
 I know that to forgive someone
 can be far from being an easy option, and I know that forgiveness
@@ -336,8 +415,10 @@ to break the cycle
 of any hatred, resentment or bitterness, always resisting evil
 and conquering it with goodness.
 Bring your healing and peace and wholeness into the lives of those I pray for,
-and into mine. Amen.`,
-`Loving Lord,
+and into mine. Amen.
+  `,
+  `
+Loving Lord,
 I often see on the TV news examples of inhumanity to others - people being tortured, abused, injured or killed.
 I need to remind myself
 that the commandment “do not kill” also refers to my attitude
@@ -348,8 +429,10 @@ if I ignore them
 or cut them off
 or do them down.
 Loving Lord, inspire me
-to take care of the smaller things of life as well as the bigger issues. Amen.`,
-`God our Father,
+to take care of the smaller things of life as well as the bigger issues. Amen.
+  `,
+  `
+God our Father,
 inspire us with a great respect for all human life
 from the time of the child growing in the womb
 to the point of death.
@@ -360,8 +443,10 @@ knowing that, where one person suffers and is degraded,
 all of humanity is belittled and abused.
 May we grow in a sense of love and care for those less fortunate than ourselves, and lead us to do something
 about the troubles in our world.
-Amen.`,
-`Father and lover of life,
+Amen.
+  `,
+  `
+Father and lover of life,
 you know the depths of my innermost self, and you understand me.
 You protect me on every side, shielding me from all harm.
 When you put me together in my mother’s womb, you knew all about me.
@@ -369,8 +454,10 @@ I thank you for the wonder of myself, and I stand in awe
 at all that you have made.
 As you know and love me,
 so may I come to know and love you.
-Guide me in your ways.`,
-`God our Father,
+Guide me in your ways.
+  `,
+  `
+God our Father,
 may the vastness of your creation
 that we can begin to see through a telescope, remind us of the abundance of your love.
 May the lowliness
@@ -384,8 +471,10 @@ to be one of us.
 In all that we observe,
 open our eyes
 so that we may really see
-and grow in wonder and appreciation. Amen.`,
-`O Most High, all-powerful, good Lord God, to you belong praise, glory,
+and grow in wonder and appreciation. Amen.
+  `,
+  `
+O Most High, all-powerful, good Lord God, to you belong praise, glory,
 honour and all blessing.
 Be praised, my Lord, for all your creation and especially for our Brother Sun,
 who brings us the day and the light;
@@ -413,8 +502,10 @@ in peace and patience
 Be praised, my Lord, for our Sister Death, whom we must all face.
 I praise and bless you, Lord,
 and I give thanks to you,
-and I will serve you in all humility.`,
-`Lord, our God and King, your greatness is seen throughout the earth.
+and I will serve you in all humility.
+  `,
+  `
+Lord, our God and King, your greatness is seen throughout the earth.
 When I gaze at the heavens
 which your fingers have formed,
 and look at the moon and the stars which you have set there,
@@ -426,8 +517,10 @@ over all the works of your hand - animals both wild and tame, birds in the air,
 and the creatures of the sea.
 Lord, our God and King,
 your greatness is seen
-throughout the earth.`,
-`Lord our God, how great you are, and I give thanks to you.
+throughout the earth.
+  `,
+  `
+Lord our God, how great you are, and I give thanks to you.
 You stretch out the heavens like a tent, with the sun to mark our days of work and the moon for our nights of rest.
 Your fingers created the earth
 and wrapped it with the ocean like a cloak.
@@ -448,8 +541,10 @@ that they could never be counted.
 What variety you have created, Lord, arranging everything so wisely!
 You send your Spirit, and all things have life. Fill us with your Spirit, Lord,
 and give us new life,
-and renew the earth that you love.`,
-`God our Father,
+and renew the earth that you love.
+  `,
+  `
+God our Father,
 we give you thanks
 for the talents you have given to mankind, and for the blessings received
 through those who use their gifts well.
@@ -478,8 +573,10 @@ the things that contradict your love,
 and may all your people
 work and grow together
 as brothers and sisters,
-building up your kingdom on earth. Amen.`,
-`Lord, it’s good
+building up your kingdom on earth. Amen.
+  `,
+  `
+Lord, it’s good
 to be actively concerned
 about the abuses of human rights
 in other countries,
@@ -506,8 +603,10 @@ that those who think differently from me can hold equally valid views.
 In these and other ways, Lord,
 may I grow in respect for all people.
 This day, may others respect me as much as I respect them.
-Amen.`,
-`Lord, we remember before you
+Amen.
+  `,
+  `
+Lord, we remember before you
 all our brothers and sisters
 who are weighed down with suffering.
 Bless and guide us
@@ -515,13 +614,16 @@ that your love may be reflected in our concern for the hungry, the oppressed and
 Help us to acknowledge
 and grow in appreciation
 that all people are made
-in your image and likeness. Amen.`,
-`Make us worthy, Lord,
+in your image and likeness. Amen.
+  `,
+  `
+Make us worthy, Lord,
 to serve our brothers and sisters throughout the world
 who live and die in poverty and hunger. Give them by our hands
 this day their daily bread,
 and by our understanding love
-give peace and joy. Amen.`,
+give peace and joy. Amen.
+  `,
   `
 Make us worthy, Lord,
 to serve our brothers and sisters
